416fdafb1d9ac761965dc45b5d5dd42b92d67605
KoofNG <israelakpan296@gmail.com>
2019-01-28 22:00:19 +0100

    Designed the table to show records from the db

--- admin/reports.php
+++ admin/reports.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome - Reports</title>
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/main.css">    
     <!-- Javascript Dependencies -->
     <script src="../assets/js/vue.js"></script>    
     <script src="../assets/js/axios.min.js"></script>
@@ -28,33 +28,44 @@
                 <div id="mainEnv">
                     <h3 class="section-header">Existing Database Records</h3>
 
-                    <div class="table-responsive">
-                        <table id="dbRecords" class="table">
+                    <div class="table">
+                        <table>
                             <thead>
                                 <tr>
-                                    <th>Census No</th> 
-                                    <th>Full Name</th>
-                                    <th>Date of Birth</th>
-                                    <th>Gender</th>
-                                    <th>Ethnic Group</th>
-                                    <th>State of Origin</th>
-                                    <th>LGA</th>
-                                    <th>Hometown</th>
-                                    <th>Religion</th>
-                                    <th>Phone Number</th>
+                                    <th class='image'><p>Picture</p></th>
+                                    <th class='card'><p>Census Card No.</p></th>
+                                    <th class='name'><p>Full Name</p></th>
+                                    <th class='combo'><p>Gender/Age</p></th>
+                                    <th class='religion'><p>Religion</p></th>
+                                    <th class='soo'><p>State of Origin</p></th>
+                                    <th class='actions'><p>Action</p></th>
                                 </tr>
                             </thead>
-
                             <tbody>
-                            
+                                <tr class="table-row" v-for="user in userRecords" :key="user.userId">
+                                    <td class='image'>
+                                        <div class="userImage">
+                                            <img src="../assets/images/koof.jpg" alt="">
+                                        </div>
+                                    </td>
+                                    <td class='card'><p class="cNumber">ONDOCC{{user.userId}}</p></td>
+                                    <td class='name'><p class="title">{{user.fullName}}</p></td>
+                                    <td class='combo'><p class="title">{{user.gender}},{{user.age}}years</p></td>
+                                    <td class='religion'><p class="title">{{user.religion}}</p></td>
+                                    <td class='soo'><p class="title">{{user.stateOfOrigin}}</p></td>
+                                    <td class='actions'><div class="moreBtn"></div></td>
+                                </tr>                        
                             </tbody>
-                        </table>
+                        </table>                 
                     </div>
+
+                    
                 </div>
             </div>
         </div>
     </div>
 
     <script src="../assets/js/main.js"></script>
+    <script src='../admin/scripts/populateRecords.js'></script>
 </body>
 </html>
