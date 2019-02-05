2e678c92a1b2dd54e85a296271d1a3e21f511c35
KoofNG <israelakpan296@gmail.com>
2019-02-05 14:50:57 +0100

    Added Vue Transition effect, added control effect to the search form

--- admin/scripts/updateRecords.js
+++ admin/scripts/updateRecords.js
@@ -1,70 +1,110 @@
 let vm = new Vue({
-    el: '#app',
-    data: {
-        fullName: '',
-        dob: '',
-        gender: '',
-        ethnicGroup: '',
-        stateOfOrigin: '',
-        lga: '',
-        hometown: '',
-        stateOfResidence: '',
-        lgaOfResidence: '',
-        religion: '',
-        occupation: '',
-        phoneNumber: '',
-        email: '',
+  el: "#app",
+  data: {
+    fullName: "",
+    dob: "",
+    gender: "",
+    ethnicGroup: "",
+    stateOfOrigin: "",
+    lga: "",
+    hometown: "",
+    stateOfResidence: "",
+    lgaOfResidence: "",
+    religion: "",
+    occupation: "",
+    phoneNumber: "",
+    email: "",
 
-        updatedInfo: '',
-        cardNo: '',
-        noRecords: true,
-        recordsFound: false,
-    },
-    methods: {
-        changeViews: function () {
-            let vtabs = document.querySelectorAll('.updateForm a');
-            for (let i = 0; i < vtabs.length; i++) {
-                vtabs[i].addEventListener('click', function (event) {
-                    var e = event.target.id;
-                    document.querySelector('.updateForm a.active').classList.remove('active');
-                    document.querySelector('div.active').classList.remove('active');
-                    this.classList.add('active');
-                    document.querySelector('div#' + e).classList.add('active');
-                });
-            }
-        },
+    updatedInfo: "",
+    cardNo: "",
+    noRecords: true,
+    recordsFound: false,
+    show: true,
+    ondoLGA: [
+        "Akoko North-East", "Akoko North-West", "Akoko South-East",
+        "Akoko South-West", "Akure North", "Akure South",
+        "Ese Odo", "Idanre", "Ifedore", "Ilaje", "Ile Oluji/Okeigbo", "Irele",
+        "Odigbo", "Okitipupa", "Ondo East", "Ondo West", "Ose", "Owo"],
+    AvailableStates: [
+        "Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno",
+        "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu", "FCT", "Gombe", "Imo",
+        "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa",
+        "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara"
+    ],
+  },
 
-        findUser: function () {
-            console.log(this.cardNo);
-            axios.get('http://localhost/Centralized-Census-Management-System/api/users/readSingle.php?userId=' + this.cardNo)
-                .then(response => {
-                    this.fullName = response.data.fullName;
-                    this.dob = response.data.dob;
-                    this.gender = response.data.gender;
-                    this.stateOfOrigin = response.data.stateOfOrigin;
-                    this.lga = response.data.lga;
-                    this.hometown = response.data.hometown;
-                    this.stateOfResidence = response.data.stateOfResidence;
-                    this.lgaOfResidence = response.data.lgaOfResidence;
-                    this.ethnicGroup = response.data.ethnicGroup;
-                    this.religion = response.data.religion;
-                    this.occupation = response.data.occupation;
-                    this.phoneNumber = response.data.phoneNumber;
-                    this.email = response.data.email;
-                    this.noRecords = false;
-                    this.recordsFound = true;
-                })
-                .catch(err => console.log(err));
-        }
+  methods: {
+    changeViews: function() {
+      let vtabs = document.querySelectorAll(".updateForm a");
+      for (let i = 0; i < vtabs.length; i++) {
+        vtabs[i].addEventListener("click", function(event) {
+          var e = event.target.id;
+          document
+            .querySelector(".updateForm a.active")
+            .classList.remove("active");
+          document.querySelector("div.active").classList.remove("active");
+          this.classList.add("active");
+          document.querySelector("div#" + e).classList.add("active");
+        });
+      }
     },
 
-    mounted() {
-        this.$nextTick(function () {
-            this.changeViews();
-        })
-    },
+    findUser: function() {
+      if (this.cardNo.length != 0) {
+        document.getElementById("loader").classList.add("show");
+        axios
+          .get(
+            "http://localhost/Centralized-Census-Management-System/api/users/readSingle.php?userId=" +
+              this.cardNo
+          )
+          .then(response => {
+            console.log(response);
+            this.fullName = response.data.fullName;
+            this.dob = response.data.dob;
+            this.gender = response.data.gender;
+            this.stateOfOrigin = response.data.stateOfOrigin;
+            this.lga = response.data.lga;
+            this.hometown = response.data.hometown;
+            this.stateOfResidence = response.data.stateOfResidence;
+            this.lgaOfResidence = response.data.lgaOfResidence;
+            this.ethnicGroup = response.data.ethnicGroup;
+            this.religion = response.data.religion;
+            this.occupation = response.data.occupation;
+            this.phoneNumber = response.data.phoneNumber;
+            this.email = response.data.email;
+            setTimeout(() => {
+              document.getElementById("loader").classList.remove("show");
+              this.noRecords = false;
+              this.recordsFound = true;
+            }, 1000);
+          })
+          .catch(err => console.log(err));
+      } else {
+        document.getElementById("alertBox").classList.add("active");
+        this.noRecords = true;
+        this.recordsFound = false;
+        setTimeout(() => {
+          document.getElementById("alertBox").classList.remove("active");
+        }, 1500);
+        document.querySelector("#cardno").classList.add("error");
+      }
+    }
+  },
 
-    updated() {
-        this.changeViews();
-    },
+  mounted() {
+    this.$nextTick(function() {
+      this.changeViews();
+    });
+  },
+
+  updated() {
+    this.changeViews();
+  },
+
+  watch: {
+    cardNo: function() {
+      return (document.querySelector("#cardno").classList.remove("error"));
+    }
+  }
+
 });
--- admin/update.php
+++ admin/update.php
@@ -24,12 +24,14 @@
             <div id="sidebar">
                 <?php require_once('./pages/navigation.php')?>
             </div> 
-            <div id="rightbar">            
+            <div id="rightbar">                 
+                <div id="loader"></div>    
+                <div id="alertBox"><p id='error'>Provide Census card number</p></div>    
                 <div id="mainEnv">
                     <div class="searchForms">
                         <p class="heading">Find User Record</p>
                         <div class='form'>
-                            <div class="userName">
+                           <div class="userName">
                                 <input type="text" name="lastname" id="lastname" placeholder='Last Name'>
                             </div>
                             <div class="userCardNo">
@@ -37,111 +39,122 @@
                             </div> 
                             <div id="buttons">
                                 <button id='searchUser' @click='findUser' type="submit">Search <span></span></button>
-                            </div>                           
+                            </div>                        
                         </div>
                     </div>
-
                     <div class="mainDocs">
-                        <p v-if='noRecords' id="info">No records yet</p>
+                        <p v-if='noRecords' class="heading" id="info">No records yet</p>
+                        <transition name='slide-fade'>
+                            <div v-if='recordsFound' class="updateForm">
+                                <p class="heading">User record found</p>
+                                <div class="create_new">
+                                    <ul>
+                                        <li><a id="personal" class='active'>personal information</a></li>
+                                        <li><a id="location">location information</a></li>
+                                        <li><a id="general">general information</a></li>
+                                        <li><a id="national">national information</a></li>
+                                    </ul>
+                                </div>
+                                <div id='personal' class='active'>
+                                    <div>
+                                        <label for="">full name</label>
+                                        <input type="text" v-model='fullName'>
+                                    </div>
+                                    <div>
+                                        <label for="">date of birth</label>
+                                        <input type="date" v-model='dob'>
+                                    </div>
+                                    <div>
+                                        <label for="">gender</label>
+                                        <select name="gender" id="" v-model='gender'>
+                                            <option value="" disabled>Choose gender</option>
+                                            <option value="Female">Female</option>
+                                            <option value="Male">Male</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label for="">home address</label>
+                                        <input type="text">
+                                    </div>
+                                </div>
+                                <div id="location">
+                                    <div>
+                                        <label for="">state of origin</label>
+                                        <select name="" v-model='stateOfOrigin' id="">
+                                            <option value disabled>Choose State</option>
+                                            <option v-for="(item, index) in AvailableStates" :key="index" v-bind:value="item">{{item}}</option>
 
-                        <div v-if='recordsFound' class="updateForm">
-                            <div class="create_new">
-                                <ul>
-                                    <li><a id="personal" class='active'>personal information</a></li>
-                                    <li><a id="location">location information</a></li>
-                                    <li><a id="general">general information</a></li>
-                                    <li><a id="national">national information</a></li>
-                                </ul>
-                            </div>
-                            <div id='personal' class='active'>
-                                <div>
-                                    <label for="">full name</label>
-                                    <input type="text" v-model='fullName'>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label for="">local government area</label>
+                                        <select name="" v-model='lga' id=""></select>
+                                    </div>
+                                    <div>
+                                        <label for="">home town</label>
+                                        <input type="text" v-model='hometown'>
+                                    </div>
+                                    <div>
+                                        <label for="">state of residence</label>
+                                        <select name="" id="" v-model='stateOfResidence'>
+                                            <option value disabled>Choose State</option>
+                                            <option value="Ondo">Ondo State</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label for="">local goverment area of residence</label>
+                                        <select name="" id="" v-model='lgaOfResidence'>                                            
+                                            <option value disabled>Choose LGA</option>
+                                            <option v-for="(item, index) in ondoLGA" :key="index" v-bind:value="item">{{item}}</option>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div>
-                                    <label for="">date of birth</label>
-                                    <input type="date" v-model='dob'>
+                                <div id="general">
+                                    <div>
+                                        <label for="">ethnic group</label>
+                                        <select name="" id="" v-model='ethnicGroup'></select>
+                                    </div>
+                                    <div>
+                                        <label for="">religion</label>
+                                        <select name="" id="" v-model='religion'></select>
+                                    </div>
+                                    <div>
+                                        <label for="">occupation</label>
+                                        <select name="" id="" v-model='occupation'></select>
+                                    </div>
+                                    <div>
+                                        <label for="">phone number</label>
+                                        <input type="tel" v-model='phoneNumber'>
+                                    </div>
+                                    <div>
+                                        <label for="">email address</label>
+                                        <input type="email" v-model='email'>
+                                    </div>
                                 </div>
-                                <div>
-                                    <label for="">gender</label>
-                                    <select name="gender" id="" v-model='gender'>
-                                        <option value="" disabled>Choose gender</option>
-                                        <option value="Female">Female</option>
-                                        <option value="Male">Male</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <label for="">home address</label>
-                                    <input type="text">
-                                </div>
-                            </div>
-                            <div id="location">
-                                <div>
-                                    <label for="">state of origin</label>
-                                    <select name="" v-model='stateOfOrigin' id=""></select>
-                                </div>
-                                <div>
-                                    <label for="">local government area</label>
-                                    <select name="" v-model='lga' id=""></select>
-                                </div>
-                                <div>
-                                    <label for="">home town</label>
-                                    <input type="text" v-model='hometown'>
-                                </div>
-                                <div>
-                                    <label for="">state of residence</label>
-                                    <select name="" id="" v-model='stateOfResidence'></select>
-                                </div>
-                                <div>
-                                    <label for="">local goverment area of residence</label>
-                                    <select name="" id="" v-model='lgaOfResidence'></select>
-                                </div>
-                            </div>
-                            <div id="general">
-                                <div>
-                                    <label for="">ethnic group</label>
-                                    <select name="" id="" v-model='ethnicGroup'></select>
-                                </div>
-                                <div>
-                                    <label for="">religion</label>
-                                    <select name="" id="" v-model='religion'></select>
-                                </div>
-                                <div>
-                                    <label for="">occupation</label>
-                                    <select name="" id="" v-model='occupation'></select>
-                                </div>
-                                <div>
-                                    <label for="">phone number</label>
-                                    <input type="tel" v-model='phoneNumber'>
-                                </div>
-                                <div>
-                                    <label for="">email address</label>
-                                    <input type="email" v-model='email'>
+                                <div id="national">
+                                    <div>
+                                        <label for="">bank verification number</label>
+                                        <input type="text">
+                                    </div>
+                                    <div>
+                                        <label for="">national identification number</label>
+                                        <input type="text">
+                                    </div>
+                                    <div>
+                                        <label for="">voters identification number</label>
+                                        <input type="text">
+                                    </div>
+                                    <div>
+                                        <label for="">international passport number</label>
+                                        <input type="text">
+                                    </div>
+                                </div> 
+                                <div id="update">
+                                    <button type="submit">Update User</button>
                                 </div>
                             </div>
-                            <div id="national">
-                                <div>
-                                    <label for="">bank verification number</label>
-                                    <input type="text">
-                                </div>
-                                <div>
-                                    <label for="">national identification number</label>
-                                    <input type="text">
-                                </div>
-                                <div>
-                                    <label for="">voters identification number</label>
-                                    <input type="text">
-                                </div>
-                                <div>
-                                    <label for="">international passport number</label>
-                                    <input type="text">
-                                </div>
-                            </div> 
-                            <div id="update">
-                                <button type="submit">Update User</button>
-                            </div>
-                        </div>
-                    </div>
+                        </transition>                        
+                    </div> 
                 </div>
             </div>
         </div>
