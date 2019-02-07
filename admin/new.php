a51ffdcc6568d73c3e542d41e50f01f5f414cb92
KoofNG <israelakpan296@gmail.com>
2019-02-07 11:23:00 +0100

    Added Databinding with Vue to the forms

--- admin/new.php
+++ admin/new.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome - Birth Records </title>
+    <title>Welcome - New Records </title>
     <link rel="stylesheet" href="../assets/css/main.css">
     <!-- Scriptings -->
     <script src="../assets/js/vue.js"></script>    
@@ -24,7 +24,17 @@
             <div id="sidebar">
                 <?php require_once('./pages/navigation.php')?>
             </div> 
-            <div id="rightbar">            
+            <div id="rightbar">    
+                <div id="loader"></div>    
+                <div id="alertBox">
+
+                    <ul id='ul'>
+                        <li v-for="(item, index) in required" :key="index">{{item}}</li>
+                    </ul>
+                    
+                    <p id='error'>Provide Census card number</p>
+            
+                </div>            
                 <div id="mainEnv">
                     <h3 class="section-header">Create new Registrant</h3>
                     <div class='create_new'>
@@ -38,59 +48,59 @@
                             <div id="personal" class='active'>
                                 <div>
                                     <label for="fname">full name</label>
-                                    <input type="text" name="" id="" placeholder='Full Name'>
+                                    <input type="text" name="Full Name" v-model='fullName' required id="" placeholder='Full Name'>
                                 </div>
                                 <div>
                                     <label for="dob">date of birth</label>
-                                    <input type="date" name="" id="">
+                                    <input type="date" required v-model='dob' name="Date of Birth" id="">
                                 </div>
                                 <div>
                                     <label for="">age</label>
-                                    <input type="text" disabled>
+                                    <input type="text" v-model='age' disabled>
                                 </div>
                                 <div>
                                     <label for="">gender</label>
-                                    <select name="gender" id="">
-                                        <option value disabled='disabled'>Choose gender</option>
+                                    <select name="Gender" v-model='gender' required id="">
+                                        <option value='' disabled>Choose gender</option>
                                         <option value="female">Female</option>
                                         <option value="male">Male</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">home address</label>
-                                    <input type="text" name="" id="" placeholder='Home Address'>
+                                    <input type="text" v-model='homeAddress' name="Home Address" required id="" placeholder='Home Address'>
                                 </div>                            
                             </div>
                             <div id="location">
                                 <div>
                                     <label for="">state of origin</label>
-                                    <select name="state" id="">
-                                        <option value disabled='disabled'>Choose state</option>
+                                    <select name="state" v-model='stateOfOrigin' required id="">
+                                        <option value='' disabled>Choose state</option>
                                         <option value="aks">Akwa Ibom</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">local government of origin</label>
-                                    <select name="lga" id="">
-                                        <option value disabled='disabled'>Choose LGA</option>
+                                    <select name="LGA" v-model='lgaOfOrigin' required id="">
+                                        <option value='' disabled>Choose LGA</option>
                                         <option value="uyo">Uyo</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">home town</label>
-                                    <input type="text" name="" id="" placeholder='Home Town'>
+                                    <input type="text" v-model='homeTown' name="Home Town" required id="" placeholder='Home Town'>
                                 </div>  
                                 <div>
                                     <label for="">state of residence</label>
-                                    <select name="state" id="">
-                                        <option value disabled='disabled'>Choose state</option>
+                                    <select name="State of residence" v-model='stateOfResidence' required id="">
+                                        <option value='' disabled>Choose state</option>
                                         <option value="aks">Akwa Ibom</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">local government of residence</label>
-                                    <select name="r_lga" id="">
-                                        <option value disabled='disabled'>Choose LGA</option>
+                                    <select name="Resident LGA" v-model='lgaOfResidence' required id="">
+                                        <option value='' disabled>Choose LGA</option>
                                         <option value="uyo">Uyo</option>
                                     </select>
                                 </div>
@@ -98,55 +108,55 @@
                             <div id="general">
                                 <div>
                                     <label for="">ethnic group</label>
-                                    <select name="ethnicGrp" id="">
-                                        <option value disabled='disabled'>Choose ethnic group</option>
+                                    <select name="Ethnic group" required v-model='ethnicGroup' id="">
+                                        <option value='' disabled>Choose ethnic group</option>
                                         <option value="uyo">Uyo</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">religion</label>
-                                    <select name="religion" id="">
-                                        <option value disabled='disabled'>Choose religion</option>
+                                    <select name="religion" v-model='religion' required id="">
+                                        <option value='' disabled>Choose religion</option>
                                         <option value="uyo">Uyo</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">Occupation</label>
-                                    <select name="occupation" id="">
-                                        <option value disabled='disabled'>Choose occupation</option>
+                                    <select name="occupation" v-model='occupation' id="">
+                                        <option value='' disabled>Choose occupation</option>
                                         <option value="uyo">Uyo</option>
                                     </select>
                                 </div>
                                 <div>
                                     <label for="">phone number</label>
-                                    <input type="tel" placeholder='08012345678'>
+                                    <input type="tel" v-model='phoneNumber' placeholder='08012345678'>
                                 </div>
                                 <div>
                                     <label for="">email</label>
-                                    <input type="email" name="" id="" placeholder='someone@example.com'>
+                                    <input type="email" v-model='email' name="" id="" placeholder='someone@example.com'>
                                 </div>
                             </div>
                             <div id="national">
                                 <div>
                                     <label for="">bank verification number</label>
-                                    <input type="text" name="" id="" placeholder='BVN'>
+                                    <input type="text" v-model='BVN' name="" id="" placeholder='BVN'>
                                 </div>
                                 <div>
                                     <label for="">national identification number</label>
-                                    <input type="text" name="" id="" placeholder='NIN'>
+                                    <input type="text" v-model='NIN' name="" id="" placeholder='NIN'>
                                 </div>
                                 <div>
                                     <label for="">voters identification number</label>
-                                    <input type="text" name="" id="" placeholder='VIN'>
+                                    <input type="text" v-model='VIN' name="" id="" placeholder='VIN'>
                                 </div>
                                 <div>
                                     <label for="">international passport number</label>
-                                    <input type="text" name="" id="" placeholder='IPN'>
+                                    <input type="text" v-model='IPN' name="" id="" placeholder='IPN'>
                                 </div>
                             </div>
                         </div>
                         <div class="actionBtn">
-                            <button type="submit">Register</button>
+                            <button type="submit" @click='newRegistrant'>Register</button>
                         </div>
                     </div>
                 </div>
--- admin/scripts/newRecords.js
+++ admin/scripts/newRecords.js
@@ -1,20 +1,69 @@
 var vm = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    fullName: "",
+    dob: "",
+    age: "",
+    gender: "",
+    homeAddress: "",
+    stateOfOrigin: "",
+    lgaOfOrigin: "",
+    homeTown: "",
+    stateOfResidence: "",
+    lgaOfResidence: "",
+    ethnicGroup: "",
+    religion: "",
+    occupation: "",
+    phoneNumber: "",
+    email: "",
+    BVN: "",
+    NIN: "",
+    VIN: "",
+    IPN: "",
+    required: []
+  },
 
   methods: {
     changeViews: function() {
       let vtabs = document.querySelectorAll(".create_new a");
       for (let i = 0; i < vtabs.length; i++) {
         vtabs[i].addEventListener("click", function(event) {
-            event.preventDefault();     
+          event.preventDefault();
           var e = event.target.id;
           document.querySelector(".create_new a.active").classList.remove("active");
-          document.querySelector("div.active").classList.remove("active");
+          document.querySelector(".create_new div.active").classList.remove("active");
           this.classList.add("active");
           document.querySelector("div#" + e).classList.add("active");
         });
       }
+    },
+
+    newRegistrant: function() {
+      //document.getElementById("alertBox").classList.add("active");
+        var e = document.querySelectorAll("#ul>li");
+        var ul = document.getElementById("ul");
+        if(e.length != 0) {
+          setTimeout(() => {
+          for (let index = 0; index < e.length; index++) {
+            var element = e[index];
+            ul.removeChild(element);
+          }
+          document.getElementById("alertBox").classList.remove("active");
+        }, 500);
+        }
+        
+
+      var inputs = document.querySelectorAll(".create_new_form [required]");
+      inputs.forEach(element => {
+        if (element.value == "" || null) {
+          this.required.push(element.name);
+        }
+      });
+
+      console.log(this.required.length)
+
+
+      
     }
   },
 
@@ -22,5 +71,9 @@ var vm = new Vue({
     this.$nextTick(function() {
       this.changeViews();
     });
+  },
+
+  updated() {
+    this.changeViews();
   }
 });
